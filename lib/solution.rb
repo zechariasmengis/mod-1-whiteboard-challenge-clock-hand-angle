14ea9aad684890ebec5f109c3782949173ad5020
vibraniumforge <gregm9999@gmail.com>
2019-09-12 13:14:32 -0700

    added solution

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,7 @@
 def clock_angle(time)
-    # code your solution here
+	hours_int = time.split(":")[0].to_i
+    minutes_int = time.split(":")[1].to_i
+    hours_angle = (hours_int * 30 ) + (minutes_int * 0.5)
+    minutes_angle = minutes_int * 6
+     (hours_angle - minutes_angle).abs > 180 ? (360 - (hours_angle - minutes_angle)).abs : (hours_angle - minutes_angle).abs
 end
